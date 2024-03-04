1fadc5a96dcc009814fb99df1bf2402c566dbde1
KULcoder <keyuucsd@gmail.com>
2024-03-04 14:58:34 -0800

    Add the following init methods: 1. xavier 2. kaiming_uniform 3. kaiming_agop 4. kaiming_nfm

--- src/model/init_weights.py
+++ src/model/init_weights.py
@@ -6,6 +6,7 @@ from torch.distributions import MultivariateNormal
 from scipy.linalg import eigvals
 import numpy as np
 import os
+import math
 
 def init_weights(config, model):
     """
@@ -20,14 +21,23 @@ def init_weights(config, model):
         return zero_init(config, model)
     elif config['model']['init_method'] == "normal":
         return normal_init(config, model)
+    elif config['model']['init_method'] == 'xavier':
+        return xavier_init(config, model)
     elif config['model']['init_method'] == 'agop':
         return agop_init(config, model)
     elif config['model']['init_method'] == 'nfm':
         return nfm_init(config, model)
-    elif config['model']['init_method'] == 'kaiming':
-        return kaiming_init(config, model)
+    elif config['model']['init_method'] == 'kaiming_uniform':
+        return kaiming_uniform_init(config, model)
+    elif config['model']['init_method'] == 'kaiming_normal':
+        return kaiming_normal_init(config, model)
+    elif config['model']['init_method'] == 'kaiming_agop':
+        return kaiming_agop_init(config, model)
+    elif config['model']['init_method'] == 'kaiming_nfm':
+        return kaiming_nfm_init(config, model)
     else:
         raise NotImplementedError(f'Model {config["model"]["type"]} not implemented')
+    
     
 
 def zero_init(config, model):
@@ -96,9 +106,44 @@ def normal_init(config, model):
 
     return model
 
-def kaiming_init(config, model):
+def xavier_init(config, model):
     """
-    Initlize the weight of model with kaiming init method.
+    Initialize the weight with Xavier mothod.
+    """
+    if config['model']['type'] == "vgg11":
+        for name, module in model.features.named_children():
+            if isinstance(module, nn.Conv2d):
+                nn.init.xavier_normal_(module.weight)
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+    else:
+        raise NotImplementedError(f'Model {config["model"]["type"]} kaiming  init not implemented')
+    
+    return model
+
+def kaiming_uniform_init(config, model):
+    """
+    Initialize the weight with kaiming uniform init method
+    """
+    if config['model']['type'] == "vgg11":
+        for name, module in model.features.named_children():
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_uniform_(
+                    module.weight,
+                    a = 0,
+                    mode = 'fan_in',
+                    nonlinearity = 'relu'
+                )
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+    else:
+        raise NotImplementedError(f'Model {config["model"]["type"]} kaiming uniform init not implemented')
+    
+    return model
+
+def kaiming_normal_init(config, model):
+    """
+    Initlize the weight of model with kaiming normal init method.
     """
 
     if config['model']['type'] == "vgg11":
@@ -113,26 +158,30 @@ def kaiming_init(config, model):
                 if module.bias is not None:
                     nn.init.constant_(module.bias, 0)
     else:
-        raise NotImplementedError(f'Model {config["model"]["type"]} kaiming  init not implemented')
+        raise NotImplementedError(f'Model {config["model"]["type"]} kaiming normal init not implemented')
     
     return model
 
 
-def init_conv_with_cov(conv_layer, cov_matrix):
+def init_conv_with_cov(conv_layer, cov_matrix, kaiming=False):
     """
     Given the weight of a conv layer can covariance matrix, created a init with that.
+    Integrating Kaiming's control on the magnitude.
     In place operation
     """
 
     out_channel, in_channel, kernel_size, kernel_size = conv_layer.weight.data.shape
     means = torch.zeros(in_channel * kernel_size * kernel_size)
+    if kaiming:
+        coeff = calculate_coefficient(conv_layer.weight)
+        cov_matrix = coeff * cov_matrix
     distribution = MultivariateNormal(means, cov_matrix)
     # total out_channel amount of samples
     new_weights = distribution.sample((out_channel, )).reshape(out_channel, in_channel, kernel_size, kernel_size)
     new_weights.requires_grad = True
     conv_layer.weight = nn.Parameter(new_weights)
 
-def transform_matrix(A):
+def transform_matrix(A, norm=True):
     # Making positive definite
     eigenvalues, eigenvectors = np.linalg.eigh(A)
     min_eigenvalue = min(eigenvalues)
@@ -140,10 +189,11 @@ def transform_matrix(A):
         positive_eigenvalues = eigenvalues + (np.abs(min_eigenvalue) + 1e-6)
         A = eigenvectors @ np.diag(positive_eigenvalues) @ eigenvectors.T
     
-    # Normalizing by dividing by the trace
-    trace_A = np.trace(A)
-    if trace_A > 0:
-        A = A / trace_A
+    if norm:
+        # Normalizing by dividing by the trace
+        trace_A = np.trace(A)
+        if trace_A > 0:
+            A = A / trace_A
 
     return A 
 
@@ -152,17 +202,19 @@ def agop_init(config, model):
     Initialize the weight of convolution layers with agop as covariance.
     ONLY FOR VGG11
     """
-    
-    conv_layer_index = 0
-    for name, module in model.features.named_children():
-        if isinstance(module, nn.Conv2d):
-            # load the agop
-            agop_path = os.path.join(config['model']['agop_path'], f"layer_{conv_layer_index}.csv")
-            agop = np.loadtxt(agop_path, delimiter=',')
-            agop = transform_matrix(agop)
-            agop = torch.from_numpy(agop).float()
-            init_conv_with_cov(module, agop)
-            conv_layer_index += 1
+    if config['model']['type'] == "vgg11":
+        conv_layer_index = 0
+        for name, module in model.features.named_children():
+            if isinstance(module, nn.Conv2d):
+                # load the agop
+                agop_path = os.path.join(config['model']['agop_path'], f"layer_{conv_layer_index}.csv")
+                agop = np.loadtxt(agop_path, delimiter=',')
+                agop = transform_matrix(agop, norm=True)
+                agop = torch.from_numpy(agop).float()
+                init_conv_with_cov(module, agop, kaiming=False)
+                conv_layer_index += 1
+    else:
+        raise NotImplementedError(f'Model {config["model"]["type"]} agop init not implemented')
 
     return model
             
@@ -172,16 +224,93 @@ def nfm_init(config, model):
     Initialize the weight of convolution layers with nfm as covariance matrix.
     ONLY FOR VGG11
     """
-    conv_layer_index = 0
-    for name, module in model.features.named_children():
-        if isinstance(module, nn.Conv2d):
-            # load the nfm
-            nfm_path = os.path.join(config['model']['nfm_path'], f"layer_{conv_layer_index}.csv")
-            nfm = np.loadtxt(nfm_path, delimiter=',')
-            nfm = transform_matrix(nfm)
-            nfm = torch.from_numpy(nfm).float()
-            init_conv_with_cov(module, nfm)
-            conv_layer_index += 1
+    if config['model']['type'] == "vgg11":
+        conv_layer_index = 0
+        for name, module in model.features.named_children():
+            if isinstance(module, nn.Conv2d):
+                # load the nfm
+                nfm_path = os.path.join(config['model']['nfm_path'], f"layer_{conv_layer_index}.csv")
+                nfm = np.loadtxt(nfm_path, delimiter=',')
+                nfm = transform_matrix(nfm, norm=True)
+                nfm = torch.from_numpy(nfm).float()
+                init_conv_with_cov(module, nfm, kaiming=False)
+                conv_layer_index += 1
+    else:
+        raise NotImplementedError(f'Model {config["model"]["type"]} nfm init not implemented')
 
     return model
+
+def kaiming_agop_init(config, model):
+    """
+    Initialize the weight of convolution layers with agop as covariance.
+    ONLY FOR VGG11
+    """
+    if config['model']['type'] == "vgg11":
+        conv_layer_index = 0
+        for name, module in model.features.named_children():
+            if isinstance(module, nn.Conv2d):
+                # load the agop
+                agop_path = os.path.join(config['model']['agop_path'], f"layer_{conv_layer_index}.csv")
+                agop = np.loadtxt(agop_path, delimiter=',')
+                agop = transform_matrix(agop, norm=False)
+                agop = torch.from_numpy(agop).float()
+                init_conv_with_cov(module, agop, kaiming=True)
+                conv_layer_index += 1
+    else:
+        raise NotImplementedError(f'Model {config["model"]["type"]} kaiming agop init not implemented')
+
+    return model
+            
+
+def kaiming_nfm_init(config, model):
+    """
+    Initialize the weight of convolution layers with nfm as covariance matrix.
+    ONLY FOR VGG11
+    """
+    if config['model']['type'] == "vgg11":
+        conv_layer_index = 0
+        for name, module in model.features.named_children():
+            if isinstance(module, nn.Conv2d):
+                # load the nfm
+                nfm_path = os.path.join(config['model']['nfm_path'], f"layer_{conv_layer_index}.csv")
+                nfm = np.loadtxt(nfm_path, delimiter=',')
+                nfm = transform_matrix(nfm, norm=False)
+                nfm = torch.from_numpy(nfm).float()
+                init_conv_with_cov(module, nfm, kaiming=True)
+                conv_layer_index += 1
+    else:
+        raise NotImplementedError(f'Model {config["model"]["type"]} kaiming nfm init not implemented')
+    return model
+
+
+
+def calculate_coefficient(tensor):
+    """
+    Integrate Kaiming init's center idea to modify the magnitude
+    """
+    gain = math.sqrt(2) # only valid for relu activation, which we only use
+    fan = calculate_fan_in(tensor)
+
+    return gain / math.sqrt(fan)
+
+def calculate_fan_in(tensor):
+    """
+    Calculate fan_in, reference to pytorch source code
+    """
+    dimensions = tensor.dim()
+
+    if dimensions < 2:
+        raise ValueError("Fan in can not be computed for tensor with fewer than 2 dimensions")
+
+    num_input_fmaps = tensor.size(1)
+
+    receptive_field_size = 1
+    if tensor.dim() > 2:
+        for s in tensor.shape[2:]:
+            receptive_field_size *= s
+
+    fan_in = num_input_fmaps * receptive_field_size
+
+    return fan_in
+    
 
